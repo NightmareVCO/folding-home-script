14831c0877874b1a52deb0c84202ecfcf8048d09
NightmareVCO <vladimircuriel@outlook.com>
2024-10-12 21:28:24 -0400

    feat(main): add automated script-v1

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install wget curl git -y
+curl -O https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/fah-client_8.3.18_amd64.deb
+sudo dpkg -i fah-client_8.3.18_amd64.deb
+
+git clone https://github.com/NightmareVCO/folding-home-script
+cd folding-home-script
+RANDOM_NUMBER=$(shuf -i 100-999 -n 1)
+sed -i "s/<machine-name v=\"droplet .*\" \/>/<machine-name v=\"droplet $RANDOM_NUMBER\" \/>/" config.xml
+sudo cp my-config /etc/fah-client/config.xml
+sudo systemctl start fah-client
+
+
+
