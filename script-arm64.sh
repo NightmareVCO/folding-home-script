a2d759f099a6f91a6648d8d0a6beb97fa441f8f4
Vladimir Curiel <vladimircuriel@outlook.com>
2024-10-19 04:47:41 -0400

    Create script-arm64.sh

---
+++ script-arm64.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~
+sudo apt update
+sudo apt install wget git -y
+git clone https://github.com/NightmareVCO/folding-home-script.git
+curl -O https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/fah-client_8.3.18_arm64.deb
+sudo dpkg -i fah-client_8.3.18_amd64.deb
+
+cd folding-home-script
+RANDOM_NUMBER=$(shuf -i 100-999 -n 1)
+sed -i "s/<machine-name v=\"droplet .*\" \/>/<machine-name v=\"droplet $RANDOM_NUMBER\" \/>/" config.xml
+sudo cp my-config.xml /etc/fah-client/config.xml
+sudo systemctl restart fah-client
